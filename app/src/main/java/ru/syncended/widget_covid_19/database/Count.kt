55e92c7e58e7a47044e932e6e58cc451ead5f787
syncended <syncended@gmail.com>
2020-03-21 00:14:07 +0300

    Room db created

--- app/src/main/java/ru/syncended/widget_covid_19/database/Count.kt
+++ app/src/main/java/ru/syncended/widget_covid_19/database/Count.kt
@@ -1,6 +1,11 @@
-package ru.syncended.widget_covid_19.api
+package ru.syncended.widget_covid_19.database
 
-data class CountResponse (
-    val caseCount: String,
-    val date: String
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Count (
+    @ColumnInfo val caseCount: String,
+    @PrimaryKey val date: String
 )
